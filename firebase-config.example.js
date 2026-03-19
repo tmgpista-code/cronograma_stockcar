7c6488132dcd11b079b555f3841da54407f57798
tmgpista-code <cezartadau@gmail.com>
2026-03-19 09:33:19 -0300

    Delete firebase-config.example.js

--- firebase-config.example.js
+++
@@ -1,1 +0,0 @@
-export const firebaseConfig = { apiKey: "COLE_AQUI", authDomain: "COLE_AQUI", databaseURL: "COLE_AQUI", projectId: "COLE_AQUI", storageBucket: "COLE_AQUI", messagingSenderId: "COLE_AQUI", appId: "COLE_AQUI" };
