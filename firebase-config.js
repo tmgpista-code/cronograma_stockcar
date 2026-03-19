1ab607aef7adfa1a5051d49734ae1bae8f5e3666
tmgpista-code <cezartadau@gmail.com>
2026-03-19 10:27:27 -0300

    firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,9 @@
-
 export const firebaseConfig = {
   apiKey: "AIzaSyCHORjZWDycsgWVA8RU7t4f83tPiCOM7LI",
   authDomain: "cronograma-stockcar.firebaseapp.com",
+  databaseURL: "COLE_AQUI_A_DATABASE_URL",
   projectId: "cronograma-stockcar",
   storageBucket: "cronograma-stockcar.firebasestorage.app",
   messagingSenderId: "419811991881",
   appId: "1:419811991881:web:21dce3a398cff1d7b0fa98"
-
 };
-
